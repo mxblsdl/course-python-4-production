53d09a29a57aaf43d3894f5e24e6f3f41d256083
Max Blasdel <maxblasdel@gmail.com>
2023-05-25 03:19:37 +0000

    update processes

--- w3/main.py
+++ w3/main.py
@@ -3,10 +3,9 @@ from typing import List, Dict
 from tqdm import tqdm
 import os
 import multiprocessing
-from w1.data_processor import DataProcessor
+from data_processor import DataProcessor
 import constants
-from global_utils import get_file_name, make_dir, plot_sales_data
-import json
+from global_utils import get_file_name, make_dir
 import argparse
 from datetime import datetime
 from pprint import pprint
@@ -40,7 +39,9 @@ def revenue_per_region(dp: DP) -> Dict:
     for row in tqdm(data_reader_gen):
         if row[constants.OutDataColNames.COUNTRY] not in aggregate:
             aggregate[row[constants.OutDataColNames.COUNTRY]] = 0
-        aggregate[row[constants.OutDataColNames.COUNTRY]] += dp.to_float(row[constants.OutDataColNames.TOTAL_PRICE])
+        aggregate[row[constants.OutDataColNames.COUNTRY]] += dp.to_float(
+            row[constants.OutDataColNames.TOTAL_PRICE]
+        )
 
     return aggregate
 
@@ -50,13 +51,20 @@ def get_sales_information(file_path: str) -> Dict:
     dp = DP(file_path=file_path)
 
     # print stats
-    dp.describe(column_names=[constants.OutDataColNames.UNIT_PRICE, constants.OutDataColNames.TOTAL_PRICE])
+    dp.describe(
+        column_names=[
+            constants.OutDataColNames.UNIT_PRICE,
+            constants.OutDataColNames.TOTAL_PRICE,
+        ]
+    )
 
     # return total revenue and revenue per region
     return {
-        'total_revenue': dp.aggregate(column_name=constants.OutDataColNames.TOTAL_PRICE),
-        'revenue_per_region': revenue_per_region(dp),
-        'file_name': get_file_name(file_path)
+        "total_revenue": dp.aggregate(
+            column_name=constants.OutDataColNames.TOTAL_PRICE
+        ),
+        "revenue_per_region": revenue_per_region(dp),
+        "file_name": get_file_name(file_path),
     }
 
 
@@ -71,7 +79,10 @@ def batch_files(file_paths: List[str], n_processes: int) -> List[set]:
     first_set = file_paths[0:first_set_len]
     second_set = file_paths[first_set_len:]
 
-    batches = [set(file_paths[i:i + n_per_batch]) for i in range(0, len(first_set), n_per_batch)]
+    batches = [
+        set(file_paths[i : i + n_per_batch])
+        for i in range(0, len(first_set), n_per_batch)
+    ]
     for ind, each_file in enumerate(second_set):
         batches[ind].add(each_file)
 
@@ -83,7 +94,7 @@ def run(file_names: List[str], n_process: int) -> List[Dict]:
     st = time.time()
 
     print("Process : {}".format(n_process))
-    folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+    folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
     file_paths = [os.path.join(folder_path, file_name) for file_name in file_names]
     revenue_data = [get_sales_information(file_path) for file_path in file_paths]
 
@@ -103,7 +114,7 @@ def main() -> List[Dict]:
     Use the `run` method to fetch revenue data for a given batch of files
 
     Use multiprocessing module to process batches of data in parallel
-    Check `multiprocessing.Pool` and `pool.starmap` methods to help you wit the task
+    Check `multiprocessing.Pool` and `pool.starmap` methods to help you with the task
 
     At the end check the overall time taken in this code vs the time taken in W1 code
 
@@ -144,20 +155,35 @@ def main() -> List[Dict]:
     """
 
     st = time.time()
-    n_processes = 3 # you may modify this number - check out multiprocessing.cpu_count() as well
+    n_processes = (
+        3  # you may modify this number - check out multiprocessing.cpu_count() as well
+    )
 
-    parser = argparse.ArgumentParser(description="Choose from one of these : [tst|sml|bg]")
-    parser.add_argument('--type',
-                        default='tst',
-                        choices=['tst', 'sml', 'bg'],
-                        help='Type of data to generate')
+    parser = argparse.ArgumentParser(
+        description="Choose from one of these : [tst|sml|bg]"
+    )
+    parser.add_argument(
+        "--type",
+        default="tst",
+        choices=["tst", "sml", "bg"],
+        help="Type of data to generate",
+    )
     args = parser.parse_args()
 
-    data_folder_path = os.path.join(CURRENT_FOLDER_NAME, '..', constants.DATA_FOLDER_NAME, args.type)
-    files = [str(file) for file in os.listdir(data_folder_path) if str(file).endswith('csv')]
+    data_folder_path = os.path.join(
+        CURRENT_FOLDER_NAME, "..", constants.DATA_FOLDER_NAME, args.type
+    )
+    files = [
+        str(file) for file in os.listdir(data_folder_path) if str(file).endswith("csv")
+    ]
 
-    output_save_folder = os.path.join(CURRENT_FOLDER_NAME, '..', 'output', args.type,
-                                      datetime.now().strftime("%B %d %Y %H-%M-%S"))
+    output_save_folder = os.path.join(
+        CURRENT_FOLDER_NAME,
+        "..",
+        "output",
+        args.type,
+        datetime.now().strftime("%B %d %Y %H-%M-%S"),
+    )
     make_dir(output_save_folder)
     file_paths = [os.path.join(data_folder_path, file_name) for file_name in files]
 
@@ -165,15 +191,36 @@ def main() -> List[Dict]:
 
     ######################################## YOUR CODE HERE ##################################################
 
+    # Processes method (does not return result to main thread)
+    # processes = []
+    # for i in range(n_processes):
+    #     p = multiprocessing.Process(target=run, args=(batches[i], n_processes))
+    #     processes.append(p)
+
+    # for p in processes:
+    #     p.start()
+
+    # for p in processes:
+    #     p.join()
+
+    # Create args to pass to starmap
+    args = [(batch, n_processes) for batch in batches]
+
+    # Pool method (aggregaates final result to main thread)
+    with multiprocessing.Pool(processes=n_processes) as pool:
+        results = pool.starmap(run, args)
+        pool.close()
+        pool.join()
+
     ######################################## YOUR CODE HERE ##################################################
 
     en = time.time()
-    print("Overall time taken : {}".format(en-st))
+    print("Overall time taken : {}".format(en - st))
 
     # should return revenue data
-    return [{}]
+    return [results]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     res = main()
     pprint(res)
