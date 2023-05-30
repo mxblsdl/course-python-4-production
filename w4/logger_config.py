e1f7df5a52412e7ad1afea1427e8db35ee1ef083
Max Blasdel <maxblasdel@gmail.com>
2023-05-30 23:38:32 +0000

    set up logging

--- w4/logger_config.py
+++ w4/logger_config.py
@@ -13,42 +13,50 @@ class Logger:
         """
         # Create a custom logger
         self.logger = logging.getLogger(module_name)
-        make_dir(directory=os.path.join(CURRENT_FOLDER_NAME, 'logs'))
+        self.logger.setLevel(logging.DEBUG)
+        make_dir(directory=os.path.join(CURRENT_FOLDER_NAME, "logs"))
 
-        self.f_handler = logging.FileHandler(os.path.join(CURRENT_FOLDER_NAME, 'logs', log_file_name))
+        self.f_handler = logging.FileHandler(
+            os.path.join(CURRENT_FOLDER_NAME, "logs", log_file_name)
+        )
 
         # Create formatters and add it to handlers
         ######################################## YOUR CODE HERE ##################################################
         # set the logging formatter to the f_handler
+        formatter = logging.Formatter(
+            " [%(filename)s - %(lineno)s] %(asctime)s %(levelname)-8s :: %(message)s",
+            "%Y-%m-%d %H:%M:%S",
+        )
+
+        self.f_handler.setFormatter(formatter)
         ######################################## YOUR CODE HERE ##################################################
 
         ######################################## YOUR CODE HERE ##################################################
         # Add handlers to the logger and setlevel to DEBUG
+        self.f_handler.setLevel(logging.DEBUG)
+        self.logger.addHandler(self.f_handler)
         ######################################## YOUR CODE HERE ##################################################
 
     def warning(self, msg):
-        pass
+        self.logger.warn(msg)
         ######################################## YOUR CODE HERE ##################################################
         ######################################## YOUR CODE HERE ##################################################
 
     def error(self, msg):
-        pass
+        self.logger.error(msg)
         ######################################## YOUR CODE HERE ##################################################
         ######################################## YOUR CODE HERE ##################################################
 
     def info(self, msg):
-        pass
+        self.logger.info(msg)
         ######################################## YOUR CODE HERE ##################################################
         ######################################## YOUR CODE HERE ##################################################
 
     def debug(self, msg):
-        pass
+        self.logger.debug(msg)
         ######################################## YOUR CODE HERE ##################################################
         ######################################## YOUR CODE HERE ##################################################
 
 
-server_logger = Logger(log_file_name='server_logs.txt', module_name='server_logs')
-main_logger = Logger(log_file_name='main_logs.txt', module_name='main_logs')
-
-
-
+server_logger = Logger(log_file_name="server_logs.txt", module_name="server_logs")
+main_logger = Logger(log_file_name="main_logs.txt", module_name="main_logs")
